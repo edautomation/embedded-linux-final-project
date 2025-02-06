9fbd512138812f88e8cfd1f9c14b1f4a5013c7c0
Emile Decosterd <edautomation20@gmail.com>
2025-02-06 11:38:26 +0100

    Refactoring

--- serial_control/serial_control.c
+++ serial_control/serial_control.c
@@ -14,7 +14,6 @@
 #define MAX_VALUE_LENGTH 20
 #define MAX_CMD_LENGTH   MAX_NAME_LENGTH + MAX_VALUE_LENGTH + 1
 #define BUFFER_SIZE      (MAX_CMD_LENGTH + 1)
-#define FILENAME         "/var/tmp/serialcontrol.txt"
 
 enum command_type_t
 {
@@ -107,6 +106,67 @@ static enum command_type_t validate_and_prepare_input(char* buffer, int length)
     return cmd_type;
 }
 
+static void add_or_update_value_for_name(size_t name_length, char* equal_sign, size_t value_length)
+{
+    char name[MAX_NAME_LENGTH];
+    memset((void*)name, 0, MAX_NAME_LENGTH);
+    strncpy(name, buffer, name_length);
+    bool is_new = true;
+    struct data_t* list_item = list_head;
+    do
+    {
+        if (0 == strncmp(list_item->name, name, name_length))
+        {
+            printf("Found name!\n");
+            is_new = false;
+            break;
+        }
+        list_item = list_item->p_next;
+    } while (NULL != list_item);
+
+    if (is_new)
+    {
+        printf("New name: %s\n", name);
+        list_item = malloc(sizeof(struct data_t));
+    }
+
+    strncpy(list_item->name, name, name_length);
+    strncpy(list_item->value, equal_sign + 1, value_length);
+    list_item->name[name_length] = '\0';
+    list_item->value[value_length] = '\0';
+    printf("Writing %s = %s\n", list_item->name, list_item->value);
+
+    if (is_new)
+    {
+        list_item->p_next = list_head;
+        list_head = list_item;
+    }
+}
+
+static void handle_write_command()
+{
+    printf("Write command\n");
+
+    char* equal_sign = strchr(buffer, '=');
+    if (NULL == equal_sign)
+    {
+        printf("Invalid format, missing =\n");
+    }
+    else
+    {
+        size_t name_length = equal_sign - buffer;
+        size_t value_length = strlen(buffer) - name_length - 1;
+        if (name_length > MAX_NAME_LENGTH || value_length > MAX_VALUE_LENGTH)
+        {
+            printf("Invalid format, name or value string too long\n");
+        }
+        else
+        {
+            add_or_update_value_for_name(name_length, equal_sign, value_length);
+        }
+    }
+}
+
 int main(int argc, char* argv[])
 {
     printf("Hello, serial control!\n");
@@ -136,62 +196,12 @@ int main(int argc, char* argv[])
             }
             else if (COMMAND_WRITE == cmd_type)
             {
-                printf("Write command\n");
-
-                char* equal_sign = strchr(buffer, '=');
-                if (NULL == equal_sign)
-                {
-                    printf("Invalid format, missing =\n");
-                }
-                else
-                {
-                    size_t name_length = equal_sign - buffer;
-                    size_t value_length = strlen(buffer) - name_length - 1;
-                    if (name_length > MAX_NAME_LENGTH || value_length > MAX_VALUE_LENGTH)
-                    {
-                        printf("Invalid format, name or value string too long\n");
-                    }
-                    else
-                    {
-                        char name[MAX_NAME_LENGTH];
-                        memset((void*)name, 0, MAX_NAME_LENGTH);
-                        strncpy(name, buffer, name_length);
-                        bool is_new = true;
-                        struct data_t* list_item = list_head;
-                        do
-                        {
-                            if (0 == strncmp(list_item->name, name, name_length))
-                            {
-                                printf("Found name!\n");
-                                is_new = false;
-                                break;
-                            }
-                            list_item = list_item->p_next;
-                        } while (NULL != list_item);
-
-                        if (is_new)
-                        {
-                            printf("New name: %s\n", name);
-                            list_item = malloc(sizeof(struct data_t));
-                        }
-
-                        strncpy(list_item->name, name, name_length);
-                        strncpy(list_item->value, equal_sign + 1, value_length);
-                        list_item->name[name_length] = '\0';
-                        list_item->value[value_length] = '\0';
-                        printf("Writing %s = %s\n", list_item->name, list_item->value);
-
-                        if (is_new)
-                        {
-                            list_item->p_next = list_head;
-                            list_head = list_item;
-                        }
-                    }
-                }
+                handle_write_command();
             }
             else
             {
-                // TODO : handle read or write command
+                printf("Unexpected path in code!\n");
+                terminate_with_error();
             }
         }
         free(buffer);

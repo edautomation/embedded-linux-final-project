68e82f8d98e0d91461d025e5a3d736db3e499d4e
Emile Decosterd <edautomation20@gmail.com>
2025-02-06 09:30:52 +0100

    Read bytes from console and print them back

--- serial_control/serial_control.c
+++ serial_control/serial_control.c
@@ -4,13 +4,21 @@
 
 #include <errno.h>
 #include <signal.h>
+#include <unistd.h>
 
-#define STDIN_FD 0
+#define MAX_CMD_LENGTH 50
+#define BUFFER_SIZE    (MAX_CMD_LENGTH + 1)
 
 static inline void terminate_normally(void)
 {
     printf("Terminating normally\n");
     exit(EXIT_SUCCESS);
+}
+
+static inline void terminate_with_error(void)
+{
+    printf("Terminating because of an error\n");
+    exit(EXIT_FAILURE);
 }
 
 static void handle_signal(int signal)
@@ -31,6 +39,22 @@ int main(int argc, char* argv[])
 
     while (true)
     {
+        char* buffer = malloc(BUFFER_SIZE * sizeof(char));
+        if (NULL == buffer)
+        {
+            terminate_with_error();
+        }
+
+        int res = read(STDIN_FILENO, buffer, MAX_CMD_LENGTH);
+        if (res < 0)
+        {
+            printf("Could not read from standard input.\n");
+        }
+        else
+        {
+            buffer[res] = '\0';  // for string handling functions
+            printf("Read %d bytes from standard input: \"%s\".\n", res, buffer);
+        }
     }
 
     return 0;

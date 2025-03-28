1de9eb0db86c944051c1a125e85760bc2cba6194
Emile Decosterd <edautomation20@gmail.com>
2025-03-28 16:55:19 +0100

    Add mapping file

--- serial_control/serial_control.c
+++ serial_control/serial_control.c
@@ -38,6 +38,15 @@ struct modbus_reg_t
     uint16_t addr;
 };
 
+struct data_t
+{
+    uint16_t* addr;
+    struct data_t* next;
+};
+
+static struct data_t list_root = {0};
+static struct data_t* list_head = &list_root;
+
 static char* buffer = NULL;
 static ht* hash_table = NULL;
 
@@ -51,6 +60,14 @@ static inline void cleanup(void)
     if (NULL != hash_table)
     {
         ht_destroy(hash_table);
+    }
+
+    struct data_t* list_item = list_head;
+    while (&list_root != list_item)
+    {
+        struct data_t* item_to_delete = list_item;
+        list_item = list_item->next;
+        free(item_to_delete);
     }
 }
 
@@ -122,8 +139,16 @@ static void handle_write_command(void)
     int n_matches = sscanf(write_command, "%30[^=]=%u", name, &value);
     if (2 == n_matches)
     {
-        LOG_DEBUG("Parsed %s=%u", name, value);
-        // TODO : find name in hash table and write corresponding register
+        struct data_t* hash_table_entry = ht_get(hash_table, name);
+        if (NULL == hash_table_entry)
+        {
+            LOG_DEBUG("Could not find \"%s\" in mapping!\n", name);
+        }
+        else
+        {
+            LOG_DEBUG("Writing %u to register \"%s\" at address %u\n", value, name, hash_table_entry->addr);
+            // TODO: write to driver
+        }
     }
     else
     {
@@ -141,38 +166,33 @@ static void handle_read_command(void)
     }
     else
     {
-        LOG_DEBUG("Request to read %s", name);
-        // TODO : find name in hash table and read corresponding register
+        struct data_t* hash_table_entry = ht_get(hash_table, name);
+        if (NULL == hash_table_entry)
+        {
+            LOG_DEBUG("Could not find \"%s\" in mapping!\n", name);
+        }
+        else
+        {
+            LOG_DEBUG("Reading register \"%s\" at address %u\n", name, hash_table_entry->addr);
+            // TODO: read from driver
+        }
     }
 }
 
-int main(int argc, char* argv[])
+static void read_map_file(const char* filename)
 {
-    LOG_DEBUG("Hello, serial control!\n");
-
-    signal(SIGINT, handle_signal);
-    signal(SIGTERM, handle_signal);
-
-    if (argc < 2)
-    {
-        LOG_DEBUG("Please specify a file with the modbus address mapping.\n");
-        LOG_DEBUG("Usage : serial_control path/to/your/file.txt\n");
-        return EXIT_SUCCESS;
-    }
-
     hash_table = ht_create();
     if (NULL == hash_table)
     {
         LOG_DEBUG("Out of memory - Could not create hash table\n");
-        return EXIT_FAILURE;
+        terminate_with_error();
     }
     else
     {
         LOG_DEBUG("Hash table created\n");
     }
 
-    const char* mapping_filename = argv[1];
-    FILE* map_file = fopen(mapping_filename, "r");
+    FILE* map_file = fopen(filename, "r");
     if (NULL == map_file)
     {
         LOG_DEBUG("Could not open mapping file. Abort.\n");
@@ -209,12 +229,16 @@ int main(int argc, char* argv[])
         {
             LOG_DEBUG("Got line: %s\n", line_buf);
             char name[MAX_NAME_LENGTH];
-            uint16_t addr;
-            int n_matches = sscanf(line_buf, "%30[^,],%u", name, &addr);
+
+            struct data_t* list_item = malloc(sizeof(struct data_t));
+            list_item->next = list_head;
+            list_head = list_item;
+
+            int n_matches = sscanf(line_buf, "%30[^,],%u", name, &list_item->addr);
             if (2 == n_matches)
             {
                 LOG_DEBUG("Line has a valid format\n");
-                if (NULL == ht_set(hash_table, name, &addr))
+                if (NULL == ht_set(hash_table, name, list_item))
                 {
                     LOG_DEBUG("Out of memory - Could not add entry to hash table\n");
                     free(line_buf);
@@ -223,7 +247,7 @@ int main(int argc, char* argv[])
                 }
                 else
                 {
-                    LOG_DEBUG("Added mapping: %s, %u\n", name, addr);
+                    LOG_DEBUG("Added mapping: %s, %u\n", name, list_item->addr);
                 }
             }
             else
@@ -235,6 +259,23 @@ int main(int argc, char* argv[])
             }
         }
     } while (len > 0);
+}
+
+int main(int argc, char* argv[])
+{
+    LOG_DEBUG("Hello, serial control!\n");
+
+    signal(SIGINT, handle_signal);
+    signal(SIGTERM, handle_signal);
+
+    if (argc < 2)
+    {
+        LOG_DEBUG("Please specify a file with the modbus address mapping.\n");
+        LOG_DEBUG("Usage : serial_control path/to/your/file.txt\n");
+        return EXIT_SUCCESS;
+    }
+
+    read_map_file(argv[1]);
 
     while (true)
     {

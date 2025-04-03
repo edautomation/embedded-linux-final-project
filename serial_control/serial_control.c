177f6d60eec59b6ccf51cf9cb726d90d96d337cc
edautomation <122728655+edautomation@users.noreply.github.com>
2025-04-03 07:58:36 +0200

    feat(app): Add mapping file and calls to kernel driver (#6)
    * Add hash table
    
    * Add mapping file
    
    * Add calls to modbus driver

--- serial_control/serial_control.c
+++ serial_control/serial_control.c
@@ -1,4 +1,5 @@
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,12 +11,15 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include "../serial_driver/serial_modbus_ioctl.h"
+#include "ht.h"
+
 #define MAX_NAME_LENGTH  30
 #define MAX_VALUE_LENGTH 20
 #define MAX_CMD_LENGTH   MAX_NAME_LENGTH + MAX_VALUE_LENGTH + 1
 #define BUFFER_SIZE      (MAX_CMD_LENGTH + 1)
 
-#define DEBUG
+#define nDEBUG
 #ifdef DEBUG
 #define LOG_DEBUG(...) printf(__VA_ARGS__)
 #else
@@ -29,16 +33,24 @@ enum command_type_t
     COMMAND_WRITE,
 };
 
-struct data_t
+struct modbus_reg_t
 {
     char name[MAX_NAME_LENGTH];
-    char value[MAX_VALUE_LENGTH];
+    uint16_t addr;
+};
+
+struct data_t
+{
+    uint16_t* addr;
     struct data_t* next;
 };
 
-static char* buffer = NULL;
 static struct data_t list_root = {0};
 static struct data_t* list_head = &list_root;
+
+static char* buffer = NULL;
+static ht* hash_table = NULL;
+static int fd = 0;
 
 static inline void cleanup(void)
 {
@@ -47,12 +59,22 @@ static inline void cleanup(void)
         free(buffer);
     }
 
+    if (NULL != hash_table)
+    {
+        ht_destroy(hash_table);
+    }
+
     struct data_t* list_item = list_head;
     while (&list_root != list_item)
     {
         struct data_t* item_to_delete = list_item;
         list_item = list_item->next;
         free(item_to_delete);
+    }
+
+    if (0 != fd)
+    {
+        close(fd);
     }
 }
 
@@ -65,7 +87,7 @@ static inline void terminate_normally(void)
 
 static inline void terminate_with_error(void)
 {
-    LOG_DEBUG("Terminating because of an error\n");
+    printf("Terminating because of an error\n");
     cleanup();
     exit(EXIT_FAILURE);
 }
@@ -89,7 +111,7 @@ static enum command_type_t validate_and_prepare_input(char* input, int length)
     enum command_type_t cmd_type = COMMAND_INVALID;
     if ('\n' != input[length - 1])
     {
-        LOG_DEBUG("Command too long\n");
+        printf("Command too long\n");
     }
     else if ('!' == input[0])
     {
@@ -101,7 +123,7 @@ static enum command_type_t validate_and_prepare_input(char* input, int length)
     }
     else
     {
-        LOG_DEBUG("Invalid start of command\n");
+        printf("Invalid start of command\n");
     }
 
     if (!(COMMAND_INVALID == cmd_type))
@@ -113,66 +135,86 @@ static enum command_type_t validate_and_prepare_input(char* input, int length)
     return cmd_type;
 }
 
-static void add_or_update_value_for_name(char* string, size_t name_length, char* equal_sign, size_t value_length)
+static int read_from_modbus(int fd, uint16_t* buf, size_t n_regs)
 {
-    char name[MAX_NAME_LENGTH];
-    memset((void*)name, 0, MAX_NAME_LENGTH);
-    strncpy(name, string, name_length);
-    bool is_new = true;
-    struct data_t* list_item = list_head;
-    do
+    size_t n_bytes = n_regs * sizeof(uint16_t);
+    int res = read(fd, buf, n_bytes);
+    if (res < 0)
     {
-        if (0 == strncmp(list_item->name, name, name_length))
-        {
-            LOG_DEBUG("Found name!\n");
-            is_new = false;
-            break;
-        }
-        list_item = list_item->next;
-    } while (NULL != list_item);
-
-    if (is_new)
-    {
-        LOG_DEBUG("New name: %s\n", name);
-        list_item = malloc(sizeof(struct data_t));
+        printf("ERR - Could not read from modbus driver: %d\n", res);
     }
-
-    strncpy(list_item->name, name, name_length);
-    strncpy(list_item->value, equal_sign + 1, value_length);
-    list_item->name[name_length] = '\0';
-    list_item->value[value_length] = '\0';
-    LOG_DEBUG("Writing %s = %s\n", list_item->name, list_item->value);
-    printf("OK\n");
-
-    if (is_new)
+    else
     {
-        list_item->next = list_head;
-        list_head = list_item;
+        printf("INFO - Read %d bytes from modbus driver: \n", res);
     }
+    return res;
+}
+
+static int write_to_modbus(int fd, uint16_t* buf, size_t n_regs)
+{
+    size_t n_bytes = n_regs * sizeof(uint16_t);
+    int res = write(fd, buf, n_bytes);
+    if (res < 0)
+    {
+        printf("ERR - Could not write to modbus driver: %d\n", res);
+    }
+    else
+    {
+        printf("INFO - Wrote %d bytes to modbus driver: \n", res);
+    }
+    return res;
+}
+
+static int set_modbus_address(int fd, unsigned long address)
+{
+    int res = ioctl(fd, SERIAL_MODBUSCHAR_IOCSETADDR, &address);
+    if (res < 0)
+    {
+        printf("ERR - Could not set modbus address %lu. Reason: %d\n", address, res);
+    }
+    else
+    {
+        printf("INFO - Set modbus address to %lu\n", address);
+    }
+    return res;
 }
 
 static void handle_write_command(void)
 {
     LOG_DEBUG("Write command\n");
 
-    char* write_command = buffer + 1;
-    char* equal_sign = strchr(write_command, '=');
-    if (NULL == equal_sign)
+    char* write_command = buffer + 1;  // ignore the '!' from now on.
+
+    char name[MAX_NAME_LENGTH];
+    uint32_t value = 0;
+    int n_matches = sscanf(write_command, "%30[^=]=%u", name, &value);
+    if (2 == n_matches)
     {
-        LOG_DEBUG("Invalid format, missing =\n");
-    }
-    else
-    {
-        size_t name_length = equal_sign - write_command;
-        size_t value_length = strlen(write_command) - name_length - 1;
-        if (name_length > MAX_NAME_LENGTH || value_length > MAX_VALUE_LENGTH)
+        if (value > UINT16_MAX)
         {
-            LOG_DEBUG("Invalid format, name or value string too long\n");
+            printf("Invalid value (bigger than UINT16_MAX)\n");
         }
         else
         {
-            add_or_update_value_for_name(write_command, name_length, equal_sign, value_length);
+            struct data_t* hash_table_entry = ht_get(hash_table, name);
+            if (NULL == hash_table_entry)
+            {
+                printf("Could not find \"%s\" in mapping!\n", name);
+            }
+            else
+            {
+                printf("Writing %u to register \"%s\" at address %u\n", value, name, hash_table_entry->addr);
+                if (set_modbus_address(fd, hash_table_entry->addr) >= 0)
+                {
+                    uint16_t value_16bits = (uint16_t)value;
+                    write_to_modbus(fd, &value_16bits, 1);
+                }
+            }
         }
+    }
+    else
+    {
+        printf("Invalid format!\n");
     }
 }
 
@@ -182,34 +224,137 @@ static void handle_read_command(void)
     size_t name_length = strlen(name);
     if (name_length > MAX_NAME_LENGTH)
     {
-        LOG_DEBUG("Invalid format, name string too long!\n");
+        printf("Invalid format, name string too long!\n");
     }
     else
     {
-        struct data_t* list_item = list_head;
-        do
+        struct data_t* hash_table_entry = ht_get(hash_table, name);
+        if (NULL == hash_table_entry)
         {
-            if (0 == strcmp(list_item->name, name))
+            printf("Could not find \"%s\" in mapping!\n", name);
+        }
+        else
+        {
+            printf("Reading register \"%s\" at address %u\n", name, hash_table_entry->addr);
+            if (set_modbus_address(fd, hash_table_entry->addr) >= 0)
             {
-                printf("%s\n", list_item->value);
-                break;
+                uint16_t value = 0;
+                read_from_modbus(fd, &value, 1);
+                printf("\"%s\" = %u\n", name, value);
             }
-            list_item = list_item->next;
-        } while (NULL != list_item);
-
-        if (NULL == list_item)
-        {
-            printf("NO ENTRY\n");
         }
     }
 }
 
+static void read_map_file(const char* filename)
+{
+    hash_table = ht_create();
+    if (NULL == hash_table)
+    {
+        printf("Out of memory - Could not create hash table\n");
+        terminate_with_error();
+    }
+    else
+    {
+        LOG_DEBUG("Hash table created\n");
+    }
+
+    FILE* map_file = fopen(filename, "r");
+    if (NULL == map_file)
+    {
+        printf("Could not open mapping file. Abort.\n");
+        terminate_with_error();
+    }
+    else
+    {
+        LOG_DEBUG("File open\n");
+    }
+
+    char* line_buf = NULL;
+    size_t buf_len = 0;
+    ssize_t len = 0;
+    size_t line_nr = 0;
+    do
+    {
+        line_nr++;
+        len = getline(&line_buf, &buf_len, map_file);
+        if (len < 0)
+        {
+            LOG_DEBUG("Could not read line or finished reading file\n");
+            break;
+        }
+        else if (len > (MAX_NAME_LENGTH + MAX_VALUE_LENGTH))
+        {
+            printf("Invalid line in map file: l.%u\n", line_nr);
+            printf("Line too long: %u\n", len);
+            free(line_buf);
+            fclose(map_file);
+            terminate_with_error();
+        }
+        else
+        {
+            LOG_DEBUG("Got line: %s\n", line_buf);
+            char name[MAX_NAME_LENGTH];
+
+            struct data_t* list_item = malloc(sizeof(struct data_t));
+            list_item->next = list_head;
+            list_head = list_item;
+
+            int n_matches = sscanf(line_buf, "%30[^,],%u", name, &list_item->addr);
+            if (2 == n_matches)
+            {
+                LOG_DEBUG("Line has a valid format\n");
+                if (NULL == ht_set(hash_table, name, list_item))
+                {
+                    printf("Out of memory - Could not add entry to hash table\n");
+                    free(line_buf);
+                    fclose(map_file);
+                    terminate_with_error();
+                }
+                else
+                {
+                    LOG_DEBUG("Added mapping: %s, %u\n", name, list_item->addr);
+                }
+            }
+            else
+            {
+                printf("Invalid format in line: %s!\n", line_buf);
+                free(line_buf);
+                fclose(map_file);
+                terminate_with_error();
+            }
+        }
+    } while (len > 0);
+    free(line_buf);
+    fclose(map_file);
+}
+
 int main(int argc, char* argv[])
 {
-    LOG_DEBUG("Hello, serial control!\n");
+    printf("Hello, serial control!\n");
 
     signal(SIGINT, handle_signal);
     signal(SIGTERM, handle_signal);
+
+    if (argc < 2)
+    {
+        printf("Please specify a file with the modbus address mapping.\n");
+        printf("Usage : serial_control path/to/your/file.txt\n");
+        return EXIT_SUCCESS;
+    }
+
+#ifndef DUMMY_DRIVER
+    fd = open("/dev/serial_modbus", O_RDWR);
+#else
+    fd = open("/var/tmp/dummy_modbus_drv.txt", O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+#endif
+    if (fd < 0)
+    {
+        printf("ERR - Could not open serial modbus driver\n");
+        return EXIT_FAILURE;
+    }
+
+    read_map_file(argv[1]);
 
     while (true)
     {
